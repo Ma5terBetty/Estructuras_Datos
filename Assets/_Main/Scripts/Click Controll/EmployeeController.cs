023db6b7d395dd9a21d15063c634d72d79499332
Nicolas Massara <nicolasmassara@outlook.com>
2022-08-29 15:46:59 -0300

    - Click controll added (is not finished at all, looks horrible)
    - Employee Tag added

---
+++ Assets/_Main/Scripts/Click Controll/EmployeeController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmployeeController : MonoBehaviour
+{
+    [SerializeField] private Camera worldCamera;
+    private IControllable employee;
+
+
+    private void Update()
+    {
+        if(employee == null)
+            SelectEmployee();
+        else
+            SelectDestination();
+    }
+
+    private void SelectEmployee()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = worldCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                if (hitInfo.collider.gameObject.CompareTag("Employee"))
+                {
+                    Debug.Log("Employee Found!");
+                    if (hitInfo.collider.TryGetComponent(out IControllable newEmployee))
+                    {
+                        employee = newEmployee;
+                    }
+                    else Debug.Log("IControllable component was not found");
+                }
+                else
+                    Debug.Log("Nothing");
+            }
+        }
+    }
+
+    private void SelectDestination()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = worldCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo, 100f))
+            {
+                employee.SetTargetPosition(hitInfo.point);
+                employee = null;
+            }
+        }
+    }
+}
