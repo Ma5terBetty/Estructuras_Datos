f5cd8a8dacf713455b0253aaacc70fb5a755db92
Tomás Sossich <tomassossich77@gmail.com>
2022-09-04 19:26:11 -0300

    Product Requests UI Elements Added

--- Assets/_Main/Scripts/UIManager.cs
+++ Assets/_Main/Scripts/UIManager.cs
@@ -1,19 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
 
 public class UIManager : MonoBehaviour
 {
+    public bool dontDestroyOnLoad;
+
     public static UIManager Instance;
+
+    float testTimer = 0;
+
+    //private GameObject UIOrder;
+    //string localPath = "Assets/_Main/UI/Order.prefab"
+
+    [SerializeField]
+    GameObject bottomLayout;
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+
+            if (dontDestroyOnLoad)
+            { 
+                DontDestroyOnLoad(gameObject);
+            }
         }
         else
         { 
             Destroy(gameObject);
+        }
+
+        bottomLayout = transform.GetChild(0).gameObject;
+
+        if (UIOrder() == null)
+        {
+            Debug.Log("La puta");
+        }
+        else
+        {
+            Debug.Log("Success");
         }
     }
 
@@ -25,6 +53,23 @@ public class UIManager : MonoBehaviour
     
     void Update()
     {
+        testTimer += Time.deltaTime;
+
+        if (testTimer > 3)
+        { 
+            var temp = Instantiate(UIOrder());
+            temp.transform.SetParent(bottomLayout.transform);
+            testTimer = 0;
+        }
+    }
+
+    public void AddItemsRequest()
+    { 
         
+    }
+
+    private GameObject UIOrder()
+    { 
+        return Resources.Load("Order", typeof(GameObject)) as GameObject;
     }
 }
