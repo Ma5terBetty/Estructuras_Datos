1a0bc24e434d7528a538df4b159965a750bf5cbd
Tomás Sossich <tomassossich77@gmail.com>
2022-09-04 17:08:20 -0300

    UI System implementation initialized

---
+++ Assets/_Main/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        { 
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
