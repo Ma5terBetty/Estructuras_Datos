d57131bf107a90428da57aff1c8b7e3594d0a106
Tomás Sossich <tomassossich77@gmail.com>
2022-09-11 22:50:52 -0300

    Order generator for UI added

--- Assets/_Main/Scripts/Order.cs
+++ Assets/_Main/Scripts/Order.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Order : MonoBehaviour
 {
+    public int redAmount;
+    public int greenAmount;
+    public int blueAmount;
+    public int yellowAmount;
     
+    public Order(int redBoxes, int greenBoxes, int blueBoxes, int yellowBoxes)
+    { 
+        redAmount = redBoxes;
+        greenAmount = greenBoxes;
+        blueAmount = blueBoxes;
+        yellowAmount = yellowBoxes;
+    }
 }
--- Assets/_Main/Scripts/OrdersManager.cs
+++ Assets/_Main/Scripts/OrdersManager.cs
@@ -1,16 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OrdersManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    GameObject rightUIPanel;
+    [SerializeField]
+    GameObject orderUIPrefab;
 
+    float timer = 0f;
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > 2.5f)
+        {
+            GenerateOrder();
+            timer = 0f;        
+        }
+    }
+    void CheckOrder()
+    {
+
+    }
+
+    void GenerateOrder()
+    {
+        int[] temp = new int[4];
+        for (int i = 0; i < temp.Length; i++)
+        { 
+            temp[i] = Mathf.RoundToInt(Random.Range(0, 4));
+        }
+        Order orderData = new Order(temp[0], temp[1], temp[2], temp[3]);
+
+        GameObject order = Instantiate(orderUIPrefab);
+
+        order.GetComponent<OrderUI>().data = orderData;
+        order.GetComponent<OrderUI>().SetInfo();
+        order.transform.SetParent(rightUIPanel.transform);
+        order.transform.localScale = Vector3.one;
+
     }
 }
